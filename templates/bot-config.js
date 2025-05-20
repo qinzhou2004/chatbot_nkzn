b3de66e35a8eef2284e72bcda504d77305766d02
qinzhou2004 <imsmurfingonu254@gmail.com>
2025-05-20 21:17:08 +0200

    Initial commit

---
+++ templates/bot-config.js
@@ -0,0 +1,30 @@
+// Auto-generated config
+module.exports = {
+  "pageTitle": "chatbot_nkzn",
+  "mainHeading": "Soporte NKZN",
+  "subHeading": "Expertos en tecnología!",
+  "inputPlaceholder": "Escribe tu mensaje aquí...",
+  "submitButtonText": "Enviar",
+  "welcomeMessage": "¡Hola! Bienvenido al NKZN Service. ¿En qué puedo ayudarte hoy?",
+  "errorMessage": "Disculpa, estoy teniendo problemas. ¿Podrías intentarlo de nuevo?",
+  "initialSuggestions": [
+    "Pregunta frecuente 1",
+    "Pregunta frecuente 2",
+    "Cómo contactar soporte"
+  ],
+  "showBranding": true,
+  "enableVoiceInput": false,
+  "responseDelay": 1000,
+  "cssConfig": {
+    "primaryColor": "#0070f3",
+    "secondaryColor": "#00dddd",
+    "messageRadius": "18px",
+    "inputRadius": "20px",
+    "showTypingIndicator": true,
+    "chatWidth": "400px",
+    "chatHeight": "600px",
+    "fontFamily": "'Helvetica Neue', sans-serif",
+    "fontSize": "14px",
+    "darkMode": false
+  }
+}
